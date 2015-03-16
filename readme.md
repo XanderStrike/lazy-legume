d50a989af68d0d9d5e2188884dfb90b28d79f7a8
Alexander Standke <xanderstrike@gmail.com>
2015-03-15 20:13:03 -0700

    init

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Lazy Legume
+
+_Because sofa spud was taken..._
+
+A simple TV show manager based on RSS feeds from torrent trackers.
---
+++ scan.rb
@@ -0,0 +1,66 @@
+require 'rss'
+require 'open-uri'
+
+class Rule
+  attr :title, :quality
+
+  def initialize title, quality
+    @title = title
+    @quality = quality
+  end
+
+  def to_regex
+    "(#{@title}.*#{@quality})"
+  end
+
+  def self.to_regex rules_list
+    /#{rules_list.map(&:to_regex).join('|')}/i
+  end
+end
+
+class Episode
+  attr :name, :season, :ep_in_season, :link
+
+  def initialize name, link
+    @name = name
+    @link = link
+    parse_season_code
+  end
+
+  def parse_season_code
+    code = @name.scan(/S\d{2}E\d{2}/i).first.scan(/\d{2}/)
+    @season = code.first.to_i
+    @ep_in_season = code.last.to_i
+  end
+
+  def to_s
+    "Season #{@season} Episode #{@ep_in_season} - #{@name}"
+  end
+end
+
+def parse f, r
+  episodes = []
+
+  open(f) do |rss|
+    feed = RSS::Parser.parse(rss)
+    puts "Title: #{feed.channel.title}"
+
+    feed.items.each do |item|
+      unless item.title.scan(r).empty?
+        episodes << Episode.new(item.title, item.link)
+      end
+    end
+  end
+
+  episodes
+end
+
+rules = [
+  Rule.new('Bobs Burgers', 'HDTV'),
+  Rule.new('World Poker Tour', 'x264'),
+]
+
+# puts Rule.to_regex(rules)
+episodes = parse(ARGV.first, Rule.to_regex(rules))
+
+puts episodes
