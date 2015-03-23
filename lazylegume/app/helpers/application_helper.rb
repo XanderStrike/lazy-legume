cf41cb06be6d638fb5b4c60f5730b00054af53d1
Alexander Standke <xanderstrike@gmail.com>
2015-03-22 17:00:23 -0700

    ui tweaks

--- lazylegume/app/helpers/application_helper.rb
+++ lazylegume/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def bootstrap_alert type, message
+    <<-HTML
+    <div class="alert alert-#{type} alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      #{message}
+    </div>
+    HTML
+  end
 end
