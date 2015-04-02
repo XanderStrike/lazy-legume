07f8e44f7eaf1ae05081475825048450883c5fef
Alexander Standke <xanderstrike@gmail.com>
2015-04-01 21:18:35 -0700

    add index page

--- lazylegume/config/routes.rb
+++ lazylegume/config/routes.rb
@@ -1,5 +1,8 @@
 Lazylegume::Application.routes.draw do
   resources :shows
   resources :feeds
-  resources :rules
+  resources :rules, only: [:new, :create, :update, :destroy]
+  resources :index, only: [:index]
+
+  root to: 'index#index'
 end
