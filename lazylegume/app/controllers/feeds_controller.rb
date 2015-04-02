5f0d2686a7132e572724a2fe8e98484a93c759ef
Alexander Standke <xanderstrike@gmail.com>
2015-04-01 22:41:26 -0700

    code repetition fixes

--- lazylegume/app/controllers/feeds_controller.rb
+++ lazylegume/app/controllers/feeds_controller.rb
@@ -17,36 +17,18 @@ class FeedsController < ApplicationController
 
   def create
     @feed = Feed.new(feed_params)
-
-    respond_to do |format|
-      if @feed.save
-        format.html { redirect_to @feed, notice: 'Feed was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @feed }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
-    end
+    @feed.save
+    redirect_to @feed, notice: 'Feed was successfully created.'
   end
 
   def update
-    respond_to do |format|
-      if @feed.update(feed_params)
-        format.html { redirect_to @feed, notice: 'Feed was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
-    end
+    @feed.update(feed_params)
+    redirect_to @feed, notice: 'Feed was successfully updated.'
   end
 
   def destroy
     @feed.destroy
-    respond_to do |format|
-      format.html { redirect_to feeds_url }
-      format.json { head :no_content }
-    end
+    redirect_to feeds_url
   end
 
   private
--- lazylegume/app/controllers/shows_controller.rb
+++ lazylegume/app/controllers/shows_controller.rb
@@ -17,36 +17,18 @@ class ShowsController < ApplicationController
 
   def create
     @show = Show.new(show_params)
-
-    respond_to do |format|
-      if @show.save
-        format.html { redirect_to @show, notice: 'Show was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @show }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
-    end
+    @show.save
+    redirect_to @show, notice: 'Show was successfully created.'
   end
 
   def update
-    respond_to do |format|
-      if @show.update(show_params)
-        format.html { redirect_to @show, notice: 'Show was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
-    end
+    @show.update(show_params)
+    redirect_to @show, notice: 'Show was successfully updated.'
   end
 
   def destroy
     @show.destroy
-    respond_to do |format|
-      format.html { redirect_to shows_url }
-      format.json { head :no_content }
-    end
+    redirect_to shows_url
   end
 
   private
