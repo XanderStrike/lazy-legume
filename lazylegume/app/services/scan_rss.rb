dc7f5239bc3d17947fee5c6a77da697c666a5560
Alexander Standke <xanderstrike@gmail.com>
2015-04-01 22:28:33 -0700

    begin work on rss parser

--- lazylegume/app/services/scan_rss.rb
+++ lazylegume/app/services/scan_rss.rb
@@ -2,21 +2,37 @@ require 'rss'
 
 class ScanRSS
   class << self
+    def find_episodes f, r
+      filter_valid_episodes(build_episodes(parse(f, r)))
+    end
+
     def parse f, r
       feed = RSS::Parser.parse(get_xml(f))
-      [].tap do |items|
+      [].tap do |results|
         feed.items.each do |item|
           unless item.title.scan(r).empty?
-            items << item
+            results << item
           end
         end
+      end
+    end
+
+    def build_episodes items
+      items.map do |item|
+        Episode.new(name: item.title, link: item.link)
+      end
+    end
+
+    def filter_valid_episodes eps
+      eps.select do |ep|
+        ep.valid?
       end
     end
 
     private
 
     def get_xml feed
-      open(feed)
+      @xml || @xml = open(feed)
     end
   end
 end
--- lazylegume/spec/services/scan_rss_spec.rb
+++ lazylegume/spec/services/scan_rss_spec.rb
@@ -1,13 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe ScanRSS, :type => :model do
+  before(:each) do
+    allow(ScanRSS).to receive(:get_xml).and_return(feed_xml)
+  end
   let(:feed_xml) { IO.read(Rails.root.join("spec", "fixtures", "sample.xml")) }
+  let(:items) { ScanRSS.parse('test_url', //) }
+
+
+  describe '#find_episodes' do
+    it 'calls the necessary methods' do
+      ScanRSS.find_episodes('test_url', /computer/i)
+    end
+  end
 
   describe '#parse' do
-    it 'returns RSS objects for each matching item' do
+    it 'returns RSS items for each matching the rss rule' do
       expect(ScanRSS).to receive(:get_xml).and_return(feed_xml)
-      expect(ScanRSS.parse('http://www.feedforall.com/sample.xml', /computer/i).map(&:title))
-        .to eq(['RSS Solutions for Computer Service Companies'])
+      expect(ScanRSS.parse('test_url', /computer/i).map(&:title))
+        .to eq(['RSS SolutioS25E33ns for Computer Service Companies'])
+    end
+  end
+
+  describe '#build_episodes' do
+    it 'returns a list of episodes' do
+      episodes = ScanRSS.build_episodes(items)
+      expect(episodes.first).to be_an(Episode)
+      expect(episodes.first.name).to eq('RSS Solutions for S01E15 Restaurants')
+      expect(episodes.first.valid?).to be_truthy
+      expect(episodes.last.valid?).to be_falsey
+    end
+  end
+
+  describe '#filter_valid_episodes' do
+    it 'removes invalid episodes' do
+      episodes = ScanRSS.build_episodes(items)
+      expect(episodes.count).to eq(9)
+      filtered_episodes = ScanRSS.filter_valid_episodes(episodes)
+      expect(filtered_episodes.count).to eq(8)
     end
   end
 end
