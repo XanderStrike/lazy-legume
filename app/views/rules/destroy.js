e03ff4c7194bb2de68b7db4ae6f088cde3ec8be4
Alexander Standke <xanderstrike@gmail.com>
2015-04-02 21:22:14 -0700

    update gitignore

--- app/views/rules/destroy.js
+++ app/views/rules/destroy.js
@@ -1,2 +1,2 @@
 $('#js-rules-area').html('<%=j render partial: "rules/rules_list", locals: { show: @show, rule: @rule } %>');
-$('#js-rules-notice').html('<%=j bootstrap_alert("danger", "Rule destroyed.").html_safe %>')
+$('#js-rules-notice').html('<%=j bootstrap_alert("danger", "Rule destroyed.").html_safe %>');
