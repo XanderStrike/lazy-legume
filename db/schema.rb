2f54ad8f5652d1cfdce2f7cb0701e1ef0ffc8c20
Alexander Standke <xanderstrike@gmail.com>
2015-06-14 13:18:27 -0700

    add keywords and quality, discourage editing regex directly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613054353) do
+ActiveRecord::Schema.define(version: 20150614193032) do
 
   create_table "episodes", force: true do |t|
     t.string   "name",                         null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20150613054353) do
     t.integer  "feed_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "keywords"
+    t.string   "quality"
   end
 
   create_table "shows", force: true do |t|
