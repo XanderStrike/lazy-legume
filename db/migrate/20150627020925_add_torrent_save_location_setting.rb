e591d97137323d6e41acb2702c75602fa0d6076e
Alexander Standke <xanderstrike@gmail.com>
2015-06-26 20:02:58 -0700

    use seeds instead of migration inserts

--- db/migrate/20150627020925_add_torrent_save_location_setting.rb
+++
@@ -1,5 +0,0 @@
-class AddTorrentSaveLocationSetting < ActiveRecord::Migration
-  def up
-    execute 'insert into settings values(1, "torrent_location", "torrents/", 0, "false")'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627020925) do
+ActiveRecord::Schema.define(version: 20150627014109) do
 
   create_table "episodes", force: true do |t|
     t.string   "name",                         null: false
